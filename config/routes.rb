400781c8559a9eddceaa4223073c04b36a11397b
Katie Wood <kwood6319@hotmail.co.uk>
2026-01-17 12:48:46 +0900

    Links between list routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'lists', to: 'lists#index'
-  get 'lists/new', to: 'lists#new', as: new_list
+  get 'lists/new', to: 'lists#new', as: 'new_list'
   post 'lists', to: 'lists#create'
-  get 'lists/:id', to 'lists#show', as :list
+  get 'lists/:id', to: 'lists#show', as: 'list'
 
 end
