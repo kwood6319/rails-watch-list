5015f68cf09ca3ba281d7ad9af725af239c16a48
Katie Wood <kwood6319@hotmail.co.uk>
2026-01-17 12:19:39 +0900

    Movie seed working... I think

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "json"
+require "open-uri"
+
+puts "Let's clean up..."
+Movie.destroy_all
+puts "All clean! #{Movie.count} movies remaining"
+
+puts "Let's find some movies"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+
+puts "Importing movie..."
+movies = JSON.parse(URI.open(url).read)["results"]
+movies.each do |movie|
+  puts "Creating #{movie["title"]}"
+  base_poster_url = "https://image.tmdb.org/t/p/original"
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "#{base_poster_url}#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+puts "Movies created"
