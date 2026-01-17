ea94170122753351e1798b8403556d219250614d
Katie Wood <kwood6319@hotmail.co.uk>
2026-01-17 11:44:35 +0900

    Models passing tests

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :movie, presence: true, uniqueness: { scope: [:movie, :list] }
+  validates :comment, presence: true, length: { minimum: 6 }
 end
